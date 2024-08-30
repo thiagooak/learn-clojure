0f10f5047aa36ca6cc582266116ac8bdd05c5693
Thiago Carvalho <thiago.oak@gmail.com>
2024-08-30 17:59:36 -0300

    Make lint happy

--- src/app/_components/repl.jsx
+++ src/app/_components/repl.jsx
@@ -18,7 +18,7 @@ export default function Repl() {
     }
 
     return (<>
-        <div className="px-2 py-1 h-3/4 max-h-3/4">{output.split("\n").map((o) => (<div>{o}</div>))}</div>
+        <div className="px-2 py-1 h-3/4 max-h-3/4">{output.split("\n").map((o, i) => (<div key={i}>{o}</div>))}</div>
         <form onSubmit={(e) => handleSubmit(e)} className="flex-row sm:flex px-2 sm:h-1/4">
             <textarea className="w-full h-full p-1 bg-gray-800" spellCheck={false} onChange={(e) => handleChange(e)}>{input}</textarea>
             <input type="submit" className="rounded bg-blue-600 py-1 px-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" value="Enter" />
