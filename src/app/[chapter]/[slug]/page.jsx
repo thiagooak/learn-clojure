c6bde217f1884bd01c7800dd29f1193f4cf13bc0
Thiago Carvalho <thiago.oak@gmail.com>
2024-09-02 10:48:57 -0300

    Move Nav to component

--- src/app/[chapter]/[slug]/page.jsx
+++ src/app/[chapter]/[slug]/page.jsx
@@ -1,24 +1,11 @@
 import { notFound } from "next/navigation";
-import { getAllChapters, getChapter, getChaptersTree } from "@/lib/api";
+import { getAllChapters, getChapter } from "@/lib/api";
 import markdownToHtml from "@/lib/markdownToHtml";
 import PostBody from "@/app/_components/post-body";
 import Repl from "@/app/_components/repl";
-
-function NavPart({path, title}) {
-  return (<li><a href={`/${path}`}>{title}</a></li>)
-}
-
-function NavChapter({path, title, children}) {
-  return (<li>
-    <a href={`/${path}`}>{title}</a>
-    <ul className="ml-4">
-      {children}
-    </ul>
-  </li>);
-}
+import Nav from "@/app/_components/nav";
 
 export default async function Chapter({ params }) {
-  const nestedChapters = getChaptersTree();
   const chapter = getChapter(params.chapter, params.slug);
 
   if (!chapter) {
@@ -30,12 +17,7 @@ export default async function Chapter({ params }) {
   return (
     <main className="flex">
         <div className="w-1/6 p-4">
-            <ul>
-              {nestedChapters.map((chapter) =>
-                (<NavChapter key={chapter.dir.slug} path={chapter.dir.slug} title={chapter.dir.title}>
-                  {chapter.pages.map((page) => (<NavPart key={page.slug} path={page.slug} title={page.title} ></NavPart>))}
-                </NavChapter>))}
-            </ul>
+            <Nav />
         </div>
         <div className="w-3/6 max-h-screen overflow-scroll">
           <div className="max-w-2xl p-4 mx-auto prose lg:prose-xl">
---
+++ src/app/_components/nav.jsx
@@ -0,0 +1,26 @@
+import { getChaptersTree } from "@/lib/api";
+
+function NavPart({path, title}) {
+    return (<li><a href={`/${path}`}>{title}</a></li>)
+  }
+
+function NavChapter({path, title, children}) {
+    return (<li>
+        <a href={`/${path}`}>{title}</a>
+        <ul className="ml-4">
+        {children}
+        </ul>
+    </li>);
+}
+
+export default function Nav() {
+  const nestedChapters = getChaptersTree();
+  return (
+    <ul>
+        {nestedChapters.map((chapter) =>
+        (<NavChapter key={chapter.dir.slug} path={chapter.dir.slug} title={chapter.dir.title}>
+            {chapter.pages.map((page) => (<NavPart key={page.slug} path={page.slug} title={page.title} ></NavPart>))}
+        </NavChapter>))}
+    </ul>
+  );
+}
